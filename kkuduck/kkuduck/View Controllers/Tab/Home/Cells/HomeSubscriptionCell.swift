c50e17fdf72853c0447a5cbd483abaf4cee516d1
betterhee <betterhee@gmail.com>
2021-11-16 20:10:16 +0900

    refactor: remove tagging

---
+++ kkuduck/kkuduck/View Controllers/Tab/Home/Cells/HomeSubscriptionCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeSubscriptionCell.swift
+//  kkuduck
+//
+//  Created by 홍다희 on 2021/11/16.
+//
+
+import UIKit
+
+final class HomeSubscriptionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var cycleLabel: UILabel!
+    @IBOutlet weak var nextDateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
