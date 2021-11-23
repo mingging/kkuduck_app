6f0ff9591e3974aee314d2d8c37edb89395cd193
min <75626237+mingging@users.noreply.github.com>
2021-11-23 22:48:22 +0900

    feat: create segment and bar chart
    세그먼트 추가 및 barchart 추가

--- kkuduck/kkuduck/View Controllers/Tab/Statistics/StatisticsViewController.swift
+++ kkuduck/kkuduck/View Controllers/Tab/Statistics/StatisticsViewController.swift
@@ -15,8 +15,12 @@ class StatisticsViewController: UIViewController {
     var writeSubInfo: NSMutableArray?
     var totalMonth: Int = 0
 
-    @IBOutlet weak var barChartView: BarChartView!
-    @IBOutlet weak var tableView: UITableView!
+    // chart view
+    @IBOutlet weak var charView: UIView!
+
+    // container view
+    @IBOutlet weak var barChartView: UIView!
+    @IBOutlet weak var pieChartView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +30,9 @@ class StatisticsViewController: UIViewController {
 
         months = ["Jun", "Jul", "Aug", "Sep", "Oct", "Nov"]
 
-        barChartView.noDataText = "데이터가 없습니다."
-        barChartView.noDataFont = .systemFont(ofSize: 20)
-        barChartView.noDataTextColor = .lightGray
+//        chartView.noDataText = "데이터가 없습니다."
+//        chartView.noDataFont = .systemFont(ofSize: 20)
+//        chartView.noDataTextColor = .lightGray
 
         setChart(dataPoints: months, values: unitsSold)
     }
@@ -37,14 +41,14 @@ class StatisticsViewController: UIViewController {
         super.viewWillAppear(animated)
 
         totalSubPrice()
-        self.tableView.reloadData()
+//        self.tableView.reloadData()
 
         setChart(dataPoints: months, values: unitsSold)
     }
 
     private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
     }
 
     func totalSubPrice() {
@@ -74,7 +78,7 @@ class StatisticsViewController: UIViewController {
                         formatter.dateFormat = "MM"
 
                         // 10월이면 더해서 10월날에 추가, 10월이 아니면 추가하지 않음 -> 10월의 차트
-                        print(month, oneMonth)
+//                        print(month, oneMonth)
                         if month == oneMonth {
                             if let octPrice = item["planPrice"] as? String {
                                 if let pp = Int(octPrice) {
@@ -107,89 +111,101 @@ class StatisticsViewController: UIViewController {
         chartDataSet.colors = [.orange]
 
         let chartData = BarChartData(dataSet: chartDataSet)
-        barChartView.data = chartData
-
-        chartDataSet.highlightEnabled = false
-        barChartView.doubleTapToZoomEnabled = false
-
-        barChartView.xAxis.labelPosition = .bottom
-        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
-
-        barChartView.xAxis.setLabelCount(dataPoints.count, force: false)
-
-        barChartView.rightAxis.enabled = false
-
-        barChartView.xAxis.drawAxisLineEnabled = false
-        barChartView.leftAxis.drawAxisLineEnabled = false
-        barChartView.leftAxis.enabled = false
-        barChartView.xAxis.drawGridLinesEnabled = false
+//        barChartView.data = chartData
+//
+//        chartDataSet.highlightEnabled = false
+//        barChartView.doubleTapToZoomEnabled = false
+//
+//        barChartView.xAxis.labelPosition = .bottom
+//        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
+//
+//        barChartView.xAxis.setLabe    lCount(dataPoints.count, force: false)
+//
+//        barChartView.rightAxis.enabled = false
+//
+//        barChartView.xAxis.drawAxisLineEnabled = false
+//        barChartView.leftAxis.drawAxisLineEnabled = false
+//        barChartView.leftAxis.enabled = false
+//        barChartView.xAxis.drawGridLinesEnabled = false
     }
 
+    // segmentControl
+
+    @IBAction func switchView(_ sender: UISegmentedControl) {
+
+        if sender.selectedSegmentIndex == 0 {
+            pieChartView.alpha = 1.0
+            barChartView.alpha = 0.0
+        } else {
+            pieChartView.alpha = 0.0
+            barChartView.alpha = 1.0
+        }
+
+    }
 }
 
-extension StatisticsViewController: UITableViewDelegate, UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let writeSubInfo = self.writeSubInfo {
-            return writeSubInfo.count
-        }
-        return 0
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-        guard let writeSubInfo = self.writeSubInfo,
-              let item = writeSubInfo[indexPath.row] as? [String:Any]
-        else { return cell }
-
-        let subName = cell.viewWithTag(1) as? UILabel
-        subName?.text = item["planName"] as? String
-
-        let planPrice = cell.viewWithTag(2) as? UILabel
-        if let totalPrice = item["planPrice"] as? String {
-            let numberFormatter = NumberFormatter()
-            numberFormatter.numberStyle = .decimal
-            planPrice?.text = "\(numberFormatter.string(for: Int(totalPrice))!)원"
-        }
-
-        // 달마다 지불되는 구독료를 계산해야 합니다유
-        // 이름을 planName의 구독료를 가져와서 이번달까지 낸 것의 총 합
-        if let startDay = item["subStartDay"] as? String { // subStartDay -> string 형태로 형변환
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy.MM.dd"
-            if let day = formatter.date(from: startDay) {
-                let calendar = Calendar.current
-                if calendar.date(byAdding: .day, value: +30, to: day) != nil {
-                    let formatter = DateFormatter()
-                    formatter.dateFormat = "yyyy.MM.dd"
-                }
-            }
-        }
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "서비스별 사용 금액"
-    }
-
-    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        guard let header = view as? UITableViewHeaderFooterView else { return }
-
-        let view: UIView = {
-            let v = UIView(frame: .infinite)
-            v.backgroundColor = .white
-
-            return v
-        }()
-
-        header.textLabel?.textColor = .black
-        header.textLabel?.isHighlighted = false
-        header.backgroundView = view
-    }
-
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
-
-}
+// extension StatisticsViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if let writeSubInfo = self.writeSubInfo {
+//            return writeSubInfo.count
+//        }
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//
+//        guard let writeSubInfo = self.writeSubInfo,
+//              let item = writeSubInfo[indexPath.row] as? [String:Any]
+//        else { return cell }
+//
+//        let subName = cell.viewWithTag(1) as? UILabel
+//        subName?.text = item["planName"] as? String
+//
+//        let planPrice = cell.viewWithTag(2) as? UILabel
+//        if let totalPrice = item["planPrice"] as? String {
+//            let numberFormatter = NumberFormatter()
+//            numberFormatter.numberStyle = .decimal
+//            planPrice?.text = "\(numberFormatter.string(for: Int(totalPrice))!)원"
+//        }
+//
+//        // 달마다 지불되는 구독료를 계산해야 합니다유
+//        // 이름을 planName의 구독료를 가져와서 이번달까지 낸 것의 총 합
+//        if let startDay = item["subStartDay"] as? String { // subStartDay -> string 형태로 형변환
+//            let formatter = DateFormatter()
+//            formatter.dateFormat = "yyyy.MM.dd"
+//            if let day = formatter.date(from: startDay) {
+//                let calendar = Calendar.current
+//                if calendar.date(byAdding: .day, value: +30, to: day) != nil {
+//                    let formatter = DateFormatter()
+//                    formatter.dateFormat = "yyyy.MM.dd"
+//                }
+//            }
+//        }
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "서비스별 사용 금액"
+//    }
+//
+//    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+//        guard let header = view as? UITableViewHeaderFooterView else { return }
+//
+//        let view: UIView = {
+//            let v = UIView(frame: .infinite)
+//            v.backgroundColor = .white
+//
+//            return v
+//        }()
+//
+//        header.textLabel?.textColor = .black
+//        header.textLabel?.isHighlighted = false
+//        header.backgroundView = view
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 50
+//    }
+// }
