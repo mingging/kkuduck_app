59f9ee6a0eab324b7b2ab8352d1b332c8be05bbc
betterhee <betterhee@gmail.com>
2021-11-29 14:33:04 +0900

    feat: modify font

--- kkuduck/kkuduck/View Controllers/Tab/List/ListViewController.swift
+++ kkuduck/kkuduck/View Controllers/Tab/List/ListViewController.swift
@@ -16,23 +16,12 @@ final class ListViewController: UIViewController {
     // MARK: - Outlets
 
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        configureTableView()
-
-        // TODO: array의 참조 변수를 넘겨주는 방법 생각해보기
-        //        writeSubInfo = NSMutableArray(contentsOfFile: getFileName("writeSubscription.plist"))
-    }
-
-    // TODO: 왜 테이블뷰에 리로드가 안 되는 것인가??? OK -> 중복코드 개선 방안 ....
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        navigationController?.setNavigationBarHidden(true, animated: false)
 
         LocalSubscriptionRepository.items { subscriptions in
             self.subscriptions = subscriptions ?? []
@@ -40,19 +29,19 @@ final class ListViewController: UIViewController {
         tableView.reloadData()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     // MARK: - Configurations
 
-    private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.backgroundColor = UIColor(named: "#FDAC53ff")
-    }
-
-    @IBAction func onChangeSegment(_ sender: UISegmentedControl) {
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
         tableView.reloadData()
     }
 
@@ -67,14 +56,7 @@ extension ListViewController: UITableViewDataSource, UITableViewDelegate {
     // 만료됨 세그먼트를 선택하면 cell에 회색의 블라인드 처리
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch segment.selectedSegmentIndex {
-        case 0:
-            return subscriptions.count
-        case 1:
-            return subscriptions.count
-        default:
-            return 0
-        }
+        return subscriptions.count
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -84,20 +66,19 @@ extension ListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ListSubscriptionCell.reuseIdentifier, for: indexPath) as! ListSubscriptionCell
 
-        switch segment.selectedSegmentIndex {
+        switch segmentedControl.selectedSegmentIndex {
         case 0:
             cell.endCellView.backgroundColor = .clear
             cell.subscriptionNameLabel.text = "네이버"
-            return cell
         case 1:
             cell.endCellView.backgroundColor = .systemGray6
             cell.endCellView.alpha = 0.5
             cell.subscriptionNameLabel.text = "디즈니"
-            return cell
         default:
-            return cell
+            break
         }
 
+        return cell
         // 데이터 불러오기
         //        guard let item = writeSubInfo?[indexPath.row] as? [String: Any] else { return cell }
         //
